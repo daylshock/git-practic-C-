aa524174cb1ac63e615e10932bd41bbdb31a6e1b
daylshock <tihon.ustujanin@yandex.ru>
2023-12-25 17:31:42 +0500

    25/12/23

--- Practic/IgnoreSymbol/IgnoreSymbol.cpp
+++ Practic/IgnoreSymbol/IgnoreSymbol.cpp
@@ -1,13 +1,25 @@
 ﻿#include <iostream>
+#include <typeinfo>
+#include <iostream>
+#include <vector>
 
-void swap(int a, int b) {
-	int tmp = a;
-	a = b;
-	b = tmp;
-	std::cout << a << " " << b;
-}
+union my_type
+{
+    double d;
+    char sym;
+    int num;
+};
 
-int main() {
-
-	return 0;
+int main()
+{
+    std::vector<my_type> mas;
+    my_type x;
+    x.d = 1.1;
+    mas.push_back(x);
+    x.sym = 's';
+    mas.push_back(x);
+    x.num = -2;
+    mas.push_back(x);
+    std::cout << "{" << mas[0].d << "," << mas[1].sym << "," << mas[2].num << "}" << std::endl;
+    return 0;
 }
---
+++ Project_5/Project_5/CommandLineArguments.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+// Комментарий к коду, сделал все 4 этапа, только на последнем выполнил не все, а именно вот это
+// "-l записать операцию помимо экрана в файл лога
+//    если использован -l -o то на экране ничего нет но 
+//	  появляются записи и в файле пользователя и в логе". Как занести аргументы, вот пример: "2 + 4 0 -o" - ноль указывает на пустой аргумент,
+// "-?" это справка о программе  "-o" это запись в файл, если надо убрать один из агрументов,
+// то просто его меняем на ноль и наоборот(по-другому я сделать не смог:( ) 
+// 
+
+int Check(double first, std::string sign, double third) {
+	if (sign == "+" || sign == "plus") { return first + third; }
+	else if (sign == "-" || sign == "subtract") { return first - third; }
+	else if (sign == "*" || sign == "multiply") { return first * third; }
+	else if (sign == "/" || sign == "share") { 
+		if (first != 0 || third != 0) {
+			return first / third;
+		} else {
+			std::cout<<"error by zero";
+			return 000;
+		} 
+	}
+	else { 
+		std::cout << "error operation\n";
+		return 1;
+	}
+}
+
+std::string Reference() {
+	return "This programm decides arithmetic operations(+-*/)";
+}
+
+void Record(double first, std::string sign, double third) {
+	std::ofstream out;
+	std::string name_file;
+	std::cout << "Enter name file?(name.txt): "<<'\n';
+	std::cin >> name_file;
+	out.open(name_file);
+	if (out.is_open()) {
+		out << first << ' ' << sign << ' ' << third << std::endl;
+		std::cout << "File is record!" << std::endl;
+	}
+	else {
+		std::cout << "The file was not found!" << std::endl;
+	}
+	out.close();
+}
+
+int main(int argC, char** argV) {
+	double first = std::stod(argV[1]);
+	std::string sign = argV[2];
+	double third = std::stod(argV[3]);
+	std::string key1 = argV[4];
+	std::string key2 = argV[5];
+	std::cout << first << " " << sign << " " << third << std::endl;
+	std::cout<<Check(first, sign, third) << std::endl;
+
+	if (key1 == "-?" ) { std::cout << Reference() << std::endl; }
+	if (key2 == "-o") { Record(first, sign, third); }
+	return 0;
+}
